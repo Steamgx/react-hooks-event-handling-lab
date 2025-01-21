abb9401b64d066d2ccf7a3aa50116fce322eb47a
Steamgx <your-bmuraya646@gmail.com>
2025-01-21 08:59:35 +0300

    first

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React, { useState } from 'react';
+
+function EyesOnMe() {
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    console.log("Good!");
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    console.log("Hey! Eyes on me!");
+    setFocused(false);
+  };
+
+  return (
+    <button
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
